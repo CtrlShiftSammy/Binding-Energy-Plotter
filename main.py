3d41da7c5d2ec71162c93133ab81b5fc38bd70bc
Kaustav B <76795611+CtrlShiftSammy@users.noreply.github.com>
2022-02-02 00:57:17 +0530

    update

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from matplotlib import pyplot as plt
 import numpy as np
+import sys
 from mpl_toolkits import mplot3d
 import pandas
 from scipy.stats import skew
@@ -9,9 +10,9 @@ from sklearn.metrics import r2_score
 from sklearn.metrics import mean_squared_error
 import math
 import seaborn as sns
-
+np.set_printoptions(threshold=sys.maxsize)
 # Import DataSet
-names = ['t1', 't2', 't3', 't4', 't5', 'experimentalBE', 'theoreticalBE']
+names = ['z', 'a', 't1', 't2', 't3', 't4', 't5', 'experimentalBE', 'theoreticalBE']
 dataSet = pandas.read_csv('data_cropped_with_formula.csv', names=names)
 
 # Linear Regression
@@ -26,8 +27,10 @@ modPred = reg.predict(x)
 print('R2(Model): ', r2_score(y,modPred)*100)
 print('RMSE(Model): ', np.sqrt(mean_squared_error(y,modPred)))
 print('RMSE(Theory): ', np.sqrt(mean_squared_error(y,z)))
-print(modPred)
-
+output_file = open("Output_data.txt", "w")
+for i in range(3558):
+    output_file.write(str(dataSet.z[i]) + " " + str(dataSet.a[i]) + " " + str(dataSet.experimentalBE[i]) + " " + str(dataSet.theoreticalBE[i]) + " " + str(modPred[i]) + "\n")
+output_file.close()
 # Graphing
 plt.style.use('ggplot')
 plt.rcParams['figure.figsize'] = (16,9)
